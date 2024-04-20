dc2983dd9cce097f1d2f6411cf859e5761a296ae
Tyler Mayoff <tyler@tylermayoff.com>
2024-04-20 18:27:55 -0400

    Fixed CI

--- src/audio.rs
+++ src/audio.rs
@@ -1,6 +1,10 @@
 use anyhow::Result;
+use ffmpeg::{
+    ffi::{swr_alloc_set_opts2, AVChannel},
+    software::scaler,
+};
 use ffmpeg_next as ffmpeg;
-use std::path::PathBuf;
+use std::{path::PathBuf, ptr};
 
 pub fn read_audio(file: PathBuf) -> Result<Vec<u8>> {
     ffmpeg::init().unwrap();
@@ -17,12 +21,27 @@ pub fn read_audio(file: PathBuf) -> Result<Vec<u8>> {
     let decoder_ctx = ffmpeg::codec::Context::from_parameters(input.parameters())?;
     let mut decoder = decoder_ctx.decoder().audio()?;
 
+    unsafe {
+        //let ctx = swr_alloc_set_opts2(
+        //    std::ptr::null(),
+        //    ffmpeg::ffi::av_get_default_channel_layout(1),
+        //    ffmpeg::ffi::AVSampleFormat::AV_SAMPLE_FMT_S16,
+        //    16000,
+        //    decoder.channel_layout(),
+        //    decoder.format(),
+        //    decoder.rate(),
+        //    0,
+        //    std::ptr::null(),
+        //);
+    }
+
     let mut receive_decoded_samples =
         |decoder: &mut ffmpeg::decoder::Audio| -> Result<(), ffmpeg::Error> {
             let mut decoded = ffmpeg::frame::audio::Audio::empty();
             while decoder.receive_frame(&mut decoded).is_ok() {
                 //let mut f = ffmpeg::frame::audio::Audio::empty();
                 let data = decoded.data(0);
+
                 audio.extend_from_slice(data);
             }
             Ok(())
